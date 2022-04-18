6fdfde495b04da50a5ac5d5f521dbaf700c511ca
Pearu Peterson <pearu.peterson@gmail.com>
2022-04-18 17:21:27 +0300

    Testing Sparse Compressed tensor features [CSR, CSC]
    ghstack-source-id: f1b20b0d71ea241add805188c8ae8b181c8a8ea2
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/75585

--- torch/testing/_internal/common_utils.py
+++ torch/testing/_internal/common_utils.py
@@ -2008,7 +2008,8 @@ class TestCase(expecttest.TestCase):
         crow_indices.cumsum_(dim=0)
         return crow_indices.to(device=device)
 
-    def genSparseCSRTensor(self, size, nnz, *, device, dtype, index_dtype):
+    def genSparseCSRTensor(self, size, nnz, *, device, dtype, index_dtype, layout=torch.sparse_csr):
+        # TODO: rename genSparseCSRTensor to genSparseCompressedTensor
         from operator import mul
         from functools import reduce
         sparse_dim = 2
@@ -2036,9 +2037,9 @@ class TestCase(expecttest.TestCase):
         crow_indices = torch.stack(next(sparse_tensors_it)).reshape(*batch_shape, -1)
         col_indices = torch.stack(next(sparse_tensors_it)).reshape(*batch_shape, -1)
 
-        return torch.sparse_csr_tensor(crow_indices,
-                                       col_indices,
-                                       values, size=size, dtype=dtype, device=device)
+        return torch.sparse_compressed_tensor(crow_indices,
+                                              col_indices,
+                                              values, size=size, dtype=dtype, layout=layout, device=device)
 
     def genSparseTensor(self, size, sparse_dim, nnz, is_uncoalesced, device, dtype):
         # Assert not given impossible combination, where the sparse dims have
